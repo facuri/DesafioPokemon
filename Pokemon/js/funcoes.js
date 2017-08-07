f6ce40eba46e9690e901bcdd795a87e2c3f1861c
manoel <mffacuri@terra.com.br>
2017-08-07 13:56:49 -0300

    Quinto commit

---
+++ Pokemon/js/funcoes.js
@@ -0,0 +1,28 @@
+function mudaFoto(foto) {
+  document.getElementById("icone").src = foto;
+ }
+var nome = "Venusaur  フシギバナ  " +
+           "\nHabilidades Especiais" +
+           "\nProteção contra fogo e gelo." +
+           "\nAuto Poder de defesa." +
+           "\nComportamento" +
+           "\nTímido"
+function ligar() {
+   alert("--------Mensagem------- " + "\n" + nome);
+ }
+var nome1 = "Charmeleon  リザード "  +
+            "\nHabilidades Especiais" +
+            "\nTemperaturas altíssimas quando balança a cauda." +
+            "\nComportamento" +
+            "\nNatureza perversa"
+function ligar1() {
+   alert("--------Mensagem------- " + "\n" + nome1);
+ }
+var nome2 = "Squirtle  ゼニガメ  "  +
+            "\nHabilidades Especiais" +
+            "\nPulverizar e jorrar água de sua boca." +
+            "\nComportamento" +
+            "\nSão muito fiéis aos seus grupos"
+function ligar2() {
+  alert("--------Mensagem------- " + "\n" + nome2);
+ }
